fa54af3d936e99431556b1227e174ffef56c43f7
ranisobha <ranisobha0541@gmail.com>
2024-02-10 17:44:46 +0530

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,67 @@
+import logo from "./orebiLogo.png";
+import logoLight from "./logoLight.png";
+import bannerImgOne from "./banner/bannerImgOne.avif";
+import bannerImgTwo from "./banner/bannerImgTwo.avif";
+import bannerImgThree from "./banner/bannerImgThree.avif";
+import saleImgOne from "./sale/saleImgOne.avif";
+import saleImgTwo from "./sale/saleImgTwo.jpg";
+import saleImgThree from "./sale/saleImgThree.jpg";
+// ============== Products Start here ====================
+// New Arrivals
+import newArrOne from "./products/newArrival/newArrOne.jpeg";
+import newArrTwo from "./products/newArrival/newArrTwo.jpeg";
+import newArrThree from "./products/newArrival/newArrThree.jpeg";
+import newArrFour from "./products/newArrival/newArrFour.jpeg";
+
+// Best Sellers
+import bestSellerOne from "./products/bestSeller/bestSellerOne.jpeg";
+import bestSellerTwo from "./products/bestSeller/bestSellerTwo.jpeg";
+import bestSellerThree from "./products/bestSeller/bestSellerThree.jpeg";
+import bestSellerFour from "./products/bestSeller/bestSellerFour.jpeg";
+
+// Special Offers
+import spfOne from "./products/specialOffer/spfOne.jpeg";
+import spfTwo from "./products/specialOffer/spfTwo.jpeg";
+import spfThree from "./products/specialOffer/spfThree.jpeg";
+import spfFour from "./products/specialOffer/spfFour.jpeg";
+
+// Year Product
+import productOfTheYear from "./products/productOfTheYear.webp";
+// ============== Products End here ======================
+import paymentCard from "./payment.png";
+import emptyCart from "../images/emptyCart.png";
+
+export {
+  logo,
+  logoLight,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  saleImgOne,
+  saleImgTwo,
+  saleImgThree,
+  // ===================== Products start here ============
+  // New Arrivals
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+
+  // Best Sellers
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+
+  // Sprcial Offers
+  spfOne,
+  spfTwo,
+  spfThree,
+  spfFour,
+
+  // Year Product
+  productOfTheYear,
+  // ===================== Products End here ==============
+  paymentCard,
+  emptyCart,
+};
